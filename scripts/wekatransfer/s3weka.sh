b638a806df4faa993d9ab2c5ff6b0854d4d1a1ad
Muennighoff <n.muennighoff@gmail.com>
2024-09-03 10:25:29 -0700

    Add scripts

---
+++ scripts/wekatransfer/s3weka.sh
@@ -0,0 +1,9 @@
+export BUDGET=ai2/oe-training
+export S3_BUCKET=ai2-llm
+#export S3_PREFIX=preprocessed/olmo-mix/danyh-compiled-v1_7
+#export S3_PREFIX=preprocessed/fastdclm
+export S3_PREFIX=preprocessed/starcoder/v1-decon-100_to_20k-2star-top_token_030
+export WEKA_BUCKET=oe-training-default
+#export WEKA_PREFIX=ai2-llm/preprocessed/danyh-compiled-v1_7
+#WEKA_PREFIX=ai2-llm/preprocessed/fastdclm
+WEKA_PREFIX=ai2-llm/preprocessed/starcoder/v1-decon-100_to_20k-2star-top_token_030
