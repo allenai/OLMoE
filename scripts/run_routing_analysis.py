499cf6058cbecb3a7a3a91f858abdd89c5b63f80
CodeCreator <wettig.alexander@gmail.com>
2024-09-03 12:31:20 -0400

    Routing analysis: Compute load balancing loss

--- scripts/run_routing_analysis.py
+++ scripts/run_routing_analysis.py
@@ -13,6 +13,35 @@ token = None
 
 start_time = time.time()
 
+
+# Adapted from transformers.models.mixtral.modeling_mixtral.load_balancing_loss_func
+# Fixed aggregating over all layers
+def load_balancing_loss_func(
+    gate_logits: torch.Tensor, num_experts: torch.Tensor = None, top_k=2
+) -> float:
+    if gate_logits is None or not isinstance(gate_logits, tuple):
+        return 0
+
+    if isinstance(gate_logits, tuple):
+        compute_device = gate_logits[0].device
+        concatenated_gate_logits = torch.stack([layer_gate.to(compute_device) for layer_gate in gate_logits], dim=1)
+
+    routing_weights = torch.nn.functional.softmax(concatenated_gate_logits, dim=-1)
+
+    _, selected_experts = torch.topk(routing_weights, top_k, dim=-1)
+
+    expert_mask = torch.nn.functional.one_hot(selected_experts, num_experts)
+
+    # Compute the percentage of tokens routed to each experts
+    tokens_per_expert = torch.mean(expert_mask.float(), dim=0)
+
+    # Compute the average probability of routing to these experts
+    router_prob_per_expert = torch.mean(routing_weights, dim=0)
+
+    overall_loss = torch.sum(tokens_per_expert * router_prob_per_expert.unsqueeze(-2)) / len(gate_logits)
+    return overall_loss * num_experts
+
+
 def load_analysis_data(tokenizer, domain, bs):
     np.random.seed(2024)
     tokens = []
@@ -38,25 +67,25 @@ def load_analysis_data(tokenizer, domain, bs):
 
 def load_sft_model():
     DEVICE = "cuda"
-    model = OlmoeForCausalLM.from_pretrained("allenai/OLMoE-1B-7B-0924-SFT", revision="no-load-balancing", token=token).to(DEVICE)
+    model = OlmoeForCausalLM.from_pretrained("allenai/OLMoE-1B-7B-0924-SFT", token=token).to(DEVICE)
     model.eval()
-    tokenizer = AutoTokenizer.from_pretrained("allenai/OLMoE-1B-7B-0924-SFT", revision="no-load-balancing", token=token)
+    tokenizer = AutoTokenizer.from_pretrained("allenai/OLMoE-1B-7B-0924-SFT", token=token)
     return model, tokenizer
 
 
 def load_dpo_model():
     DEVICE = "cuda"
-    model = OlmoeForCausalLM.from_pretrained("allenai/OLMoE-1B-7B-0924-Instruct", revision="no-load-balancing", token=token).to(DEVICE)
+    model = OlmoeForCausalLM.from_pretrained("allenai/OLMoE-1B-7B-0924-Instruct", token=token).to(DEVICE)
     model.eval()
-    tokenizer = AutoTokenizer.from_pretrained("allenai/OLMoE-1B-7B-0924-Instruct", revision="no-load-balancing", token=token)
+    tokenizer = AutoTokenizer.from_pretrained("allenai/OLMoE-1B-7B-0924-Instruct", token=token)
     return model, tokenizer
 
 
 def load_model():
     DEVICE = "cuda"
-    model = OlmoeForCausalLM.from_pretrained("OLMoE/OLMoE-1B-7B-0924", token=token).to(DEVICE)
+    model = OlmoeForCausalLM.from_pretrained("allenai/OLMoE-1B-7B-0924", token=token).to(DEVICE)
     model.eval()
-    tokenizer = AutoTokenizer.from_pretrained("OLMoE/OLMoE-1B-7B-0924", token=token)
+    tokenizer = AutoTokenizer.from_pretrained("allenai/OLMoE-1B-7B-0924", token=token)
     return model, tokenizer
 
 def load_model_mistral():
@@ -83,11 +112,20 @@ def run_analysis(domain, model_name=None):
 
     total_token_count = 0
 
+    aux_losses = []
+
     # for each expert, what are some commen tokens that are assigned to it?
     # write a code to count that and print it out: {expert_id: Counter({token1: 32, token2: 23})...}
     for i, input_ids in tqdm(enumerate(load_analysis_data(tokenizer, domain=domain, bs=length))):
         input_ids = torch.LongTensor(input_ids).reshape(1, -1).to(DEVICE)
         out = model(input_ids=input_ids, output_router_logits=True)
+
+        aux_loss = load_balancing_loss_func(
+            out["router_logits"],
+            model.num_experts,
+            model.num_experts_per_tok,
+        )
+        aux_losses.append(aux_loss.cpu().item())
 
         # input id shapes: 2048 seqlen
         input_ids = input_ids[0].detach().cpu().numpy().tolist()
@@ -126,12 +164,14 @@ def run_analysis(domain, model_name=None):
         if total_token_count > 204800:
             break
 
+    print(f"Average aux loss: {np.mean(aux_losses)}")
+
     return layer_counters, crosslayer_counters, eid2token_layer0, eid2token_layer7, eid2token_layer15
 
 name2finaldata = {"github_oss_with_stack": "github", "arxiv": "arxiv", "c4": "c4", "b3g": "book", "wikipedia": "wikipedia", "tulu": "tulu"}
 
 if __name__=='__main__':
-    model_name = "olmoe-dpo"
+    model_name = "olmoe"
     print(model_name)
     if model_name == "mistral":
         model, tokenizer = load_model_mistral()
